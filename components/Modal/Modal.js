dc815ad338554805d090b2f80d2d00d6e8bbb3ca
my name <heybassant@gmail.com>
2023-03-22 22:07:20 +0300

    merging

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -22,20 +22,17 @@ export default function Modal({openText, addLecture, deleteLecture, lectures}) {
 
   return (
     <>
-      <button onClick={() => toggleModal()} className="btn-modal border-none outline-none rounded-xl font-mirza mx-4">
-        <div className="flex items-center relative">
-          <span className="pl-6">{openText}</span>
-           {(openText === "إضافة مادة") ? <RxPlus className="absolute left-0 bottom-[25%]" /> : <TbTrash className="absolute left-0 bottom-[25%]"/>}
-        </div>
+      <button onClick={() => toggleModal()} className="btn-modal border-none outline-none rounded-xl ">
+        {openText} {(openText === "إضافة مادة") ? <RxPlus /> : <TbTrash />}
       </button>
 
       {modal && (
-        <div className="modal font-mirza overflow-y-scroll">
+        <div className="modal">
           <div onClick={() => toggleModal()} className="overlay"></div>
           <div className="modal-content">
           {(openText === "إضافة مادة") 
             ? <AddLectureForm addLecture={addLecture} toggleModal={toggleModal}/>
-            : <DeleteLectureForm deleteLecture={deleteLecture} lectures={lectures} toggleModal={toggleModal}/>
+            : <DeleteLectureForm deleteLecture={deleteLecture} lectures={lectures}/>
           }
             <button className="close-modal" onClick={() => toggleModal()}>
               <RxCross2 />
--- components/Schedule/YourSchedule.js
+++ components/Schedule/YourSchedule.js
@@ -15,13 +15,15 @@ export default function CreateSchedule() {
     }
 
   return (
-    <div className='p-6'>
-        <div className='flex flex-row justify-between items-center p-4' dir='rtl'>
-          <h2 className="mx-6 text-[#7f5ce5] text-4xl font-bold font-mirza"  dir='rtl'>جدولك في رمضان</h2>
-          <div>
-            <Modal openText={"إضافة مادة"} addLecture={addLecture}/>
-            <Modal openText={"حذف مادة"} deleteLecture={deleteLecture} lectures={lectures}/>
-          </div>
+    <div className='p-4'>
+        <div className='flex flex-row gap-[8vw] items-center' dir='rtl'>
+          <h2 className='ml-6 text-[#7f5ce5] text-6xl' dir='rtl'>جدولك في رمضان</h2>
+          <h2 className='ml-6 text-[#7f5ce6] text-6xl' dir='rtl'>شكر خاص</h2>
+          <p className='ml-6 text-[#7f5ce7]' dir='rtl'>لترى جدول محاضراتك في رمضان، كل ما عليك فعله هو أن تدخل المواعيد المعتادة لموادك، وسيظهر لك جدول بالمواعيد الجديدة. </p>
+          <p className='ml-6 text-[#7f5ce8]' dir='rtl'>لترى جدول محاضراتك في رمضان، كل ما عليك فعله هو أن تدخل المواعيد المعتادة لموادك، وسيظهر لك جدول بالمواعيد الجديدة. </p>
+          
+          <Modal openText={"إضافة مادة"} addLecture={addLecture}/>
+          <Modal openText={"حذف مادة"} deleteLecture={deleteLecture} lectures={lectures}/>
         </div>
         <Schedule lectures={lectures} />
         
