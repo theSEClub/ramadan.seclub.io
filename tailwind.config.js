9b88c9601cff8f7f601b758bc3fd7db88bb51550
Atom <albaraa456@gmail.com>
2023-03-23 16:04:06 +0300

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,30 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "green-se": {
+          100: '#e1f3f4',
+          200: '#d3f3f5',
+          DEFAULT: '#56c5c2'
+        },
+        "purple": {
+          DEFAULT: '#8754ED',
+          50: '#FCFAFE',
+          100: '#edebff',
+          200: '#D5C3F9',
+          300: '#BB9EF5',
+          400: '#A179F1',
+          500: '#8754ED',
+          600: '#6321E8',
+          700: '#4C14BD',
+          800: '#380F8A',
+          900: '#230957'
+        }
+      }
+    },
     fontFamily: {
+      'body': ['TheSans'],
       'mirza': ['"Mirza"', 'cursive'],
     },
   },
