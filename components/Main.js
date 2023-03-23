e8eae6f5caf8fe09733c5d736a593cec198f7a71
Atom <albaraa456@gmail.com>
2023-03-23 16:05:42 +0300

    fixes

--- components/Main.js
+++ components/Main.js
@@ -1,18 +1,22 @@
 import Image from 'next/image'
 import React from 'react'
 // import calendar from '../assets/calendar.svg'
-import calendar from '../assets/caln-crop.svg'
+import calendar from '../assets/calendar-ramadan.svg'
+import Container from './Container'
 
 
 
 export default function Main() {
   return (
-    <div className='main-grid px-0' style={{ position: 'relative' }}>
-        <div className='main-title flex items-end w-full justify-end'>
-          <h2 className="mx-6 text-[#7f5ce5] text-8xl font-bold font-mirza" dir='rtl'>جدولك في رمضان</h2>
+    <Container className='pt-20'>
+      <div className='flex flex-col lg:flex-row'>
+        <Image src={calendar} alt='calander' className='main-image col-start-1 w-[60vw] lg:w-[40vw] h-auto' />
+
+        <div className='flex flex-col  justify-center pt-8 lg:pt-0 lg:pb-20'>
+          <h2 className="text-[#7f5ce5] text-3xl lg:text-6xl font-bold" dir='rtl'>جدولك في رمضان</h2>
+          <p className='text-xl mt-6 text-slate-400' dir='rtl'>لترى جدول محاضراتك في رمضان، كل ما عليك فعله هو أن تدخل المواعيد المعتادة لموادك، وسيظهر لك جدول بالمواعيد الجديدة.</p>
         </div>
-        <p className='main-text text-5xl font-mirza my-0' dir='rtl'>لترى جدول محاضراتك في رمضان، كل ما عليك فعله هو أن تدخل المواعيد المعتادة لموادك، وسيظهر لك جدول بالمواعيد الجديدة.</p>
-        <Image src={calendar} alt='calander' className='main-image col-start-1 w-[50vw] h-auto' />
-    </div>
+      </div>
+    </Container>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,39 @@ import Main from '../components/Main';
 import YourSchedule from '../components/Schedule/YourSchedule';
 import styles from '@/styles/Home.module.css'
 import background from '../assets/bg.svg'
+import localFont from 'next/font/local'
 
+const sans = localFont({
+  src: [
+    {
+      path: '../assets/the-sans-arabic-regular.ttf',
+      weight: '400',
+      style: 'normal',
+      display: 'swap'
+    },
+    {
+      path: '../assets/the-sans-arabic-bold.ttf',
+      weight: '700',
+      style: 'normal',
+      display: 'swap'
+    },
+    {
+      path: '../assets/the-sans-arabic-light.ttf',
+      weight: '300',
+      style: 'normal',
+      display: 'swap'
+    }
+  ],
+})
 
 export default function Home() {
   return (
     <>
-      <div className='overflow-hidden relative'>
+      <div className={`overflow-hidden relative font-body ${sans.className}`}>
         <Image
           src={background}
           alt='calendar'
-          className='main-image col-start-1 w-[100vw] h-auto absolute top-0 left-0 z-[-1]'
+          className='main-image col-start-1 w-screen h-auto absolute top-0 left-0 z-[-1]'
         />
         <Header />
         <Main />
