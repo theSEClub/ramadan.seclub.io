c7db4777cd8e3a9b61bb92c498bfe6fb619e24be
Atom <albaraa456@gmail.com>
2023-03-23 17:09:36 +0300

    better seo

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
+import social from '../public/social.jpg'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta name="description" content="" />
-        <meta name="keywords" content="" />
+        <meta name="keywords" content="نادي هندسة البرمجيات, جامعة الامير مقرن,هندسة,هندسة البرمجيات,software development,sdlc, software engineering,club, نادي طلابي, نادي" />
         <meta name="robots" content="index, follow" />
-        <meta property="og:title" content="المساعد الرمضاني" />
+        <meta property="og:title" content="SE Club | المساعد الرمضاني" />
         <meta property="og:description" content="" />
-        <meta property="og:image" content="../public/social.jpg" />
+        <meta property="og:image" content={`https://ramadan.seclub.io/${social.jpg}`} />
         <meta property="og:url" content="https://ramadan.seclub.io" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content="المساعد الرمضاني" />
         <meta name="twitter:site" content="@SEclub_upm" />
         <meta name="twitter:domain" content="https://ramadan.seclub.io" />
         <meta name="twitter:description" content="" />
-        <meta name="twitter:image" content="../public/social.jpg" />
+        <meta name="twitter:image" content={`https://ramadan.seclub.io/${social.jpg}`} />
 
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
