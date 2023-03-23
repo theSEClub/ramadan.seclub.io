d0aa6fcb1a290d35698437e0279618f2fc1e46cf
Atom <albaraa456@gmail.com>
2023-03-23 14:03:29 +0300

    fix oveflow

--- components/Main.js
+++ components/Main.js
@@ -3,24 +3,16 @@ import React from 'react'
 // import calendar from '../assets/calendar.svg'
 import calendar from '../assets/caln-crop.svg'
 
-import background from '../assets/bg.svg'
 
 
 export default function Main() {
   return (
     <div className='main-grid px-0' style={{ position: 'relative' }}>
-
-<Image
-    src={background}
-    alt='calendar'
-    className='main-image col-start-1 w-[100vw] h-auto'
-    style={{ position: 'absolute', top: 0, left: 0, zIndex: -1 }}
-  />
-      <div className='main-title flex items-end w-full justify-end'>
-      <h2 className="mx-6 text-[#7f5ce5] text-8xl font-bold font-mirza" dir='rtl'>جدولك في رمضان</h2>
-      </div>
-      <p className='main-text text-5xl font-mirza my-0' dir='rtl'>لترى جدول محاضراتك في رمضان، كل ما عليك فعله هو أن تدخل المواعيد المعتادة لموادك، وسيظهر لك جدول بالمواعيد الجديدة.</p>
-      <Image src={calendar} alt='calander' className='main-image col-start-1 w-[50vw] h-auto' />
+        <div className='main-title flex items-end w-full justify-end'>
+          <h2 className="mx-6 text-[#7f5ce5] text-8xl font-bold font-mirza" dir='rtl'>جدولك في رمضان</h2>
+        </div>
+        <p className='main-text text-5xl font-mirza my-0' dir='rtl'>لترى جدول محاضراتك في رمضان، كل ما عليك فعله هو أن تدخل المواعيد المعتادة لموادك، وسيظهر لك جدول بالمواعيد الجديدة.</p>
+        <Image src={calendar} alt='calander' className='main-image col-start-1 w-[50vw] h-auto' />
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import YourSchedule from '../components/Schedule/YourSchedule';
 import styles from '@/styles/Home.module.css'
+import background from '../assets/bg.svg'
 
 
 export default function Home() {
   return (
     <>
-      <div>
+      <div className='overflow-hidden relative'>
+        <Image
+          src={background}
+          alt='calendar'
+          className='main-image col-start-1 w-[100vw] h-auto'
+          style={{ position: 'absolute', top: 0, left: 0, zIndex: -1 }}
+        />
         <Header />
         <Main />
         <YourSchedule />
