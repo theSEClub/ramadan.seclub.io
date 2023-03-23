512eb8d84d107b71af9c40644e3fceef317e9a76
reemini <roorscarlet@gmail.com>
2023-03-23 13:27:19 +0300

    add background

--- components/Schedule/YourSchedule.js
+++ components/Schedule/YourSchedule.js
@@ -17,7 +17,7 @@ export default function CreateSchedule() {
   return (
     <div className='p-6'>
         <div className='flex flex-row justify-between items-center p-4' dir='rtl'>
-          <h2 className="mx-6 text-[#7f5ce5] text-4xl font-bold font-mirza"  dir='rtl'>جدولك في رمضان</h2>
+          <h2 className="mx-6 text-[#7f5ce5] text-4xl font-bold font-mirza"  dir='rtl'></h2>
           <div>
             <Modal openText={"إضافة مادة"} addLecture={addLecture}/>
             <Modal openText={"حذف مادة"} deleteLecture={deleteLecture} lectures={lectures}/>
