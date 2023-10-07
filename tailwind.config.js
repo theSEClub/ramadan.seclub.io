31690301880e61c7604a7e7671e3a8c69e80f839
Ala Eddin A. Aldoukhi <75493673+Alaeddin03@users.noreply.github.com>
2023-10-07 11:53:17 +0300

    extended styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,11 @@ module.exports = {
           200: '#d3f3f5',
           DEFAULT: '#56c5c2'
         },
+        "blue-se": {
+          DEFAULT: '#a3dcfa',
+          100: "#a3dcfa",
+          200: "#4247a6",
+        },
         "purple": {
           DEFAULT: '#8754ED',
           50: '#FCFAFE',
@@ -28,7 +33,15 @@ module.exports = {
           700: '#4C14BD',
           800: '#380F8A',
           900: '#230957'
-        }
+        },
+      },
+      fontSize: {
+        '2xs': '10px',
+        '3xs': '8px',
+        '4xs': '6px',
+      },
+      gap: {
+        0.25: '1px',
       }
     },
     fontFamily: {
